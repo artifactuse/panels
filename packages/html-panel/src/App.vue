ce097257f48b02106a620236238ab0fccd0c436a
David Oti <davmixcool@gmail.com>
2026-08-14 12:46:02 +0100

    fix: adjust heading margins and sizes in markdown styles for better readability

--- packages/html-panel/src/App.vue
+++ packages/html-panel/src/App.vue
@@ -300,14 +300,14 @@ function buildMarkdownStyles(themeName) {
     }
 
     h1, h2, h3, h4, h5, h6 {
-      margin-top: 0.5em;
+      margin-top: 0.2em;
       margin-bottom: 0.5em;
       font-weight: 600;
       line-height: 1.3;
       color: ${p.heading};
     }
-    h1 { font-size: 2em; border-bottom: 2px solid ${p.border}; padding-bottom: 0.3em; }
-    h2 { font-size: 1.5em; border-bottom: 1px solid ${p.border}; padding-bottom: 0.3em; }
+    h1 { font-size: 2em; }
+    h2 { font-size: 1.5em;}
     h3 { font-size: 1.25em; }
     h4 { font-size: 1em; }
     h5 { font-size: 0.875em; }
